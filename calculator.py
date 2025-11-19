361e2940309f331c19e95c61f68bad035fd7a646
Lnovo5 <lucynovo24@gmail.com>
2025-11-19 18:27:36 -0500

    modified calculator

--- calculator.py
+++ calculator.py
@@ -7,6 +7,15 @@ calculator.py
 One function per operation, in order.
 """
 # First example
+def square_root(a):
+    try:
+        return math.sqrt(a)
+    except:
+        print("You cannot find the square root of a negative!")
+
+def hypotenuse(a, b):
+    return math.hypot(a, b)
+
 def add(a, b): 
     return a + b
 
